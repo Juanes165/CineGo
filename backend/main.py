2175deb539f4e778d9bbb81d492c4161814c70c8
Juan Esteban <betancourtjuanes@gmail.com>
2024-05-31 01:20:08 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,31 +4,33 @@ from extensions import db, jwt, getconn
 from src.routes import Auth, Movies, Users, Sales
 from dotenv import load_dotenv
 
-def create_app():
 
-    load_dotenv()
 
-    app = Flask(__name__)
-    CORS(app, origins='*')
+load_dotenv()
 
-    app.config.from_prefixed_env()
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://'
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-        'creator': getconn
-    }
+app = Flask(__name__)
+CORS(app, origins='*')
 
-    # Initialize extensions
-    db.init_app(app)
-    jwt.init_app(app)
+app.config.from_prefixed_env()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+pg8000://'
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'creator': getconn
+}
 
-    # Register auth blueprint
-    app.register_blueprint(Auth.auth_bp, url_prefix='/auth')
-    app.register_blueprint(Movies.movies_bp, url_prefix='/movies')
-    app.register_blueprint(Users.user_bp, url_prefix='/users')
-    app.register_blueprint(Sales.sale_bp, url_prefix='/sales')
+# Initialize extensions
+db.init_app(app)
+jwt.init_app(app)
 
-    @app.route('/')
-    def index():
-        return 'Welcome to the CineGo API'
+# Register auth blueprint
+app.register_blueprint(Auth.auth_bp, url_prefix='/auth')
+app.register_blueprint(Movies.movies_bp, url_prefix='/movies')
+app.register_blueprint(Users.user_bp, url_prefix='/users')
+app.register_blueprint(Sales.sale_bp, url_prefix='/sales')
 
-    return app
+@app.route('/')
+def index():
+    return 'Welcome to the CineGo API'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
